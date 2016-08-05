a423e3e0f271668ee67b60870325d412c3d07ef7
4iar <4iar@users.noreply.github.com>
2016-08-05 23:37:13 +0100

    Add endpoint for historical stock data

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 "use strict";
 const express = require('express');
 const mongodb = require('mongodb');
+const _ = require('lodash');
+const yahooFinance = require('yahoo-finance');
+
 
 // setup db
 const port = process.env.PORT || 5000;
@@ -19,14 +22,34 @@ app.use(function(req, res, next) {
     next();
 });
 
+let stocks = [];
 io.on('connection', function (socket) {
-  console.log("got a connection");
-  socket.emit('news', { hello: 'world' });
+  socket.emit('new stocks', stocks);
   
   socket.on('add stock', function (data) {
-    console.log(data);
+    if (!_.includes(stocks, data.stock)) {
+      stocks.push(data.stock);
+      io.emit('new stocks', stocks);
+    }
   });
 });
+
+app.get('/api/stocks', (request, response) => {
+  console.log(stocks)
+  yahooFinance.historical({
+    symbols: stocks,
+    from: '2012-01-01',
+    to: '2012-12-31',
+  }, (error, data) => {
+    if (error) {
+      console.log(error);
+      response.json({status: 'error', message: 'problem contacting the yahoo finance api'});
+    } else if (data) {
+      response.json({status: 'success', message: null, data});
+    }
+  });
+});
+
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/index.html');
