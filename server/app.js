a0c67efbe0073fb1ee5ed680f5b3e8990c2fcc32
4iar <4iar@users.noreply.github.com>
2016-08-05 17:58:35 +0100

    Add server scaffolding

---
+++ server/app.js
@@ -0,0 +1,27 @@
+"use strict";
+const express = require('express');
+const mongodb = require('mongodb');
+
+// setup db
+const port = process.env.PORT || 5000;
+const MongoClient = mongodb.MongoClient;
+const mongolabUri = process.env.MONGODB_URI;
+let db;
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+MongoClient.connect(mongolabUri, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(port);
+})
