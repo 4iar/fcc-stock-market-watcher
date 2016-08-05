590107572c25f0a8df94a7fe0dccdcace08c8b92
4iar <4iar@users.noreply.github.com>
2016-08-05 20:56:06 +0100

    Add form field for adding new stocks

--- client/src/components/AddStock.js
+++ client/src/components/AddStock.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 
 export default class AddStock extends React.Component {
   constructor(props) {
     super(props);
     this.socket = this.props.socket;
+    this.stockToAdd = '';
   }
 
   handleSubmit() {
-    this.socket.emit('add stock', {stock: 'AAPL'}, (data) => {
+    this.socket.emit('add stock', {stock: this.stockToAdd}, (data) => {
       console.log(data);
     })
   }
-  
+
+  handleChange(e) {
+    this.stockToAdd = e.target.value;
+  }
+
   render() {
     return (
       <div>
-        <button onClick={this.handleSubmit.bind(this)}>Add AAPL</button>
+        <TextField onChange={this.handleChange.bind(this)} floatingLabelText="Enter a stock symbol" />
+        <FlatButton onClick={this.handleSubmit.bind(this)} label="Add" primary={true} />
       </div>
     );
   }
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>header</h1>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         {this.props.children}
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
